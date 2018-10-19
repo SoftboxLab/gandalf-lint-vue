5e7f88d82db3b279ae47a417a3040c0214642d31
pablo.silva <pablo.silva@doclerholding.com>
2018-10-19 08:22:46 +0200

    updated packages, fixed imports and created test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
-    'plugin:vue/recommended',
-    'eslint-config-gandalf-lint'
+    '@softboxlab/eslint-config-gandalf-lint'
   ].map(require.resolve),
   plugins: [
     'eslint-plugin-vue'
